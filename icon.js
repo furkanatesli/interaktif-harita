169dd258082025f72061ee7d07aa397aae5e2346
Furkan Atesli <31884209+furkanatesli@users.noreply.github.com>
2020-05-27 13:18:48 +0300

    icon güncellemesi
    github io için icon güncellemesi.

--- icon.js
+++ icon.js
@@ -1,10 +1,10 @@
 	var depremicon = L.icon({
-    iconUrl: 'C:/Users/Furkan/Desktop/Harita Haber/Depremler/deprem.png',
+    iconUrl: 'Depremler/deprem.png',
     iconSize: [25, 25],
     iconAnchor: [15, 30],
     popupAnchor: [-2, -30]});
     var habericon = L.icon({
-    iconUrl: 'C:/Users/Furkan/Desktop/Harita Haber/Depremler/haber.png',
+    iconUrl: 'Depremler/haber.png',
     iconSize: [30, 30],
     iconAnchor: [15, 30],
     popupAnchor: [0, -30]});
